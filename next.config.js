f1e62d85ef5da9ce65ccd2842d2c6bebb60fb402
Mathew Luong <mathewflames@gmail.com>
2023-07-02 21:42:59 -0600

    change config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+// const nextConfig = {
+//     output: 'export',
+//     swcMinify: true,
+// }
 
-module.exports = nextConfig
+module.exports = nextConfig;
